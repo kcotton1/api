586e334522fdbce46198ea5cecd33531d351d018
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-12-01 22:41:43 -0500

    Update calculator.php

--- v1.0/calculator.php
+++ v1.0/calculator.php
@@ -3,15 +3,15 @@
         $num1 = $_GET['num1'];
         $num2 = $_GET['num2'];
         $operator = $_GET['operation'];
-		// $headers = apache_request_headers();
-		// var_dump($headers);
+		 $headers = apache_request_headers();
+		 var_dump($headers);
 	
-		// $token = $headers['Authorization'];
+		 $token = $headers['Authorization'];
 	
-		// if ($token !== 'Basic ') {
-			// http_response_code(401);
-			// exit();
-		// }
+		 if ($token !== 'Basic ') {
+			 http_response_code(401);
+			 exit();
+		 }
 		
         if ($operator === "+" || $operator === "-" || $operator === "*" || $operator === "/" || $operator === "**"){
             $n1 = floatval($num1);
