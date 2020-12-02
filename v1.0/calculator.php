7077437946e8bccb6411ddb66d157cc199aacd4d
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-12-02 03:29:54 -0500

    Add files via upload

---
+++ v1.0/calculator.php
@@ -0,0 +1,56 @@
+<?php
+$headers = apache_request_headers();
+	
+	$token = $headers['Authorization'];
+	
+	if ($token !== 'Basic punch') {
+		http_response_code(401);
+		exit();
+	}
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        $operator = $_GET['operation'];
+		}
+		
+        if ($operator === "+" || $operator === "-" || $operator === "*" || $operator === "/" || $operator === "**"){
+            $n1 = floatval($num1);
+            $n2 = floatval($num2);
+            
+            $result = Null;
+            switch($operator){
+                case "+":
+                    $result = $n1 + $n2;
+                    break;
+                case "-":
+                    $result = $n1 - $n2;
+                    break;
+                case "*":
+                    $result = $n1 * $n2;
+                    break;
+                case "/":
+                    $result = $n1 / $n2;
+                    break;
+                case "**":
+                    $result = $n1 ** $n2;
+                    break;
+            }
+            
+            if ($result !== Null){
+                echo $result;
+            } else {
+                http_response_code(500);
+            }
+			
+        }else {
+            http_response_code(400);
+            exit();
+        }
+        
+    } else if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+        exit();
+    }else {
+        http_response_code(405);
+        exit();
+    }
+?>
